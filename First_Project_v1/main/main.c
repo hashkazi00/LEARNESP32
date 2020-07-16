f96668b07015c2ba6e0df95f24b2dd8c75ce4297
hashkazi00 <sahilkazi01@hotmail.com>
2020-07-17 00:41:18 +0530

    built-gpio5-blinkey

--- First_Project_v1/main/main.c
+++ First_Project_v1/main/main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-
+#define PIN 5
 
 void app_main(void)
 {
-  printf("Hello world!\n");
-  
+  // printf("Hello world!\n");
+  gpio_pad_select_gpio(PIN);
+  gpio_set_direction(PIN, GPIO_MODE_OUTPUT);
+  int isOn = 0;
+  while (1)
+  {
+    /* code */
+    isOn = !isOn;
+    gpio_set_level(PIN, isOn);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
